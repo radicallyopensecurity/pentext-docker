bd63d9895f9c17d581d5df93746ee37a969e8fa3
Stefan Grönke <gronke@radicallyopensecurity.com>
2021-07-08 14:10:08 +0000

    quickscope2off

---
+++ quickscope2off/scripts/quickscope2off.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+cd "$CI_PROJECT_DIR"
+
+java -jar /saxon.jar  -s:source/quickscope.xml -xsl:xslt/qs2offerte.xsl -o:source/offerte.xml -xi
+
+git add source/offerte.xml
+git config user.name "GitLab CI"
+git config user.email "infra+ci@radicallyopensecurity.com"
+git commit -m "convert pentext quickscope to offerte"
+git remote -v
+
+git remote rm origin
+git remote add origin "https://CI:${PROJECT_ACCESS_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}"
+
+git push origin HEAD:${CI_DEFAULT_BRANCH}
