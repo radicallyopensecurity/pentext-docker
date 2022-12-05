08ebc2417626bb30e29912a05d2a1302bd936b81
Stefan Grönke <gronke@radicallyopensecurity.com>
2022-12-05 19:10:10 +0000

    make GIT_USER_NAME and GIT_USER_EMAIL configurable

--- off2rep/scripts/off2rep.sh
+++ off2rep/scripts/off2rep.sh
@@ -2,11 +2,13 @@
 set -e
 cd "$CI_PROJECT_DIR"
 
+export CI_SERVER_HOST="$(echo $CI_SERVER_URL | sed -e 's,^[a-zA-Z]*://\([^/@]*@\)\?\([^/@]*\).*$,\2,')"
+
 java -jar /saxon.jar  -s:source/offerte.xml -xsl:xslt/off2rep.xsl -o:source/report.xml -xi
 
 git add source/report.xml
-git config user.name "GitLab CI"
-git config user.email "infra+ci@radicallyopensecurity.com"
+git config user.name ${GIT_USER_NAME:-"GitLab CI"}
+git config user.email ${GIT_USER_EMAIL:-"infra+ci@radicallyopensecurity.com"}
 git commit -m "convert pentext offerte to report"
 git remote -v
 
